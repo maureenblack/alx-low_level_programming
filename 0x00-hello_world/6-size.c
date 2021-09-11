ef54be36a75a209f54743c8c878e0b8180b81678
Wepngong Maureen <maureenwepngong@gmail.com>
2021-09-11 19:22:06 +0100

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@ int main(void)
     char charType;
 
     // sizeof evaluates the size of a variable
-    printf("Size of a char: %zu byte\n", sizeof(charType));
-    printf("Size of an int: %zu bytes\n", sizeof(intType));
-    printf("Size of a long int: %zu bytes\n", sizeof(doubleType));
-    printf("Size of a long long int: %zu bytes\n", sizeof(doubleType));
-    printf("Size of a float: %zu bytes\n", sizeof(floatType));
+    printf("Size of a char: %zu byte\(s)\n", sizeof(charType));
+    printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+    printf("Size of a long int: %zu byte(s)\n", sizeof(doubleType));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
+    printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
  
    return 0;
 }
