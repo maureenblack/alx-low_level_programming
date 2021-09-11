353fe67b7b8874b1590d588903cd029e1a4ac6d9
Wepngong Maureen <maureenwepngong@gmail.com>
2021-09-11 17:08:32 +0100

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
